1cbd50ff863b3a614db61200aee2de96a45f4ab3
Nabiha Afrin <nabihaafrin980@gmail.com>
2024-10-03 10:26:34 +0600

    MiNi_ProJect

---
+++ learn.js
@@ -0,0 +1,39 @@
+function subcribe() {
+
+  document.getElementById('btn').innerHTML = 'Subcribed';
+  
+  document.getElementById('btn').style.backgroundColor = '#262626';
+}
+
+function like() {
+
+  document.getElementById('like').innerHTML = '<i class="fa-solid fa-thumbs-up"></i> Liked';
+
+  document.getElementById('like').style.backgroundColor = 'blue';
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
